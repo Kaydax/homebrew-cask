8338230570cc1da4ecd50b0b324e50ba6a9e62b2
Kaydax <kaydax@kaydax.xyz>
2023-12-26 16:39:05 -0500

    Test

--- Casks/p/polymc.rb
+++ Casks/p/polymc.rb
@@ -7,12 +7,6 @@ cask "polymc" do
     url "https://github.com/PolyMC/PolyMC/releases/download/#{version}/PolyMC-macOS-Legacy-#{version}.tar.gz",
         verified: "github.com/PolyMC/PolyMC/"
   end
-  on_catalina :or_newer do
-    sha256 "7fb72c2eef2900c90bc999062f85ed5e6faa60856bc7fa6e80e14c4bd4d4a61a"
-
-    url "https://github.com/PolyMC/PolyMC/releases/download/#{version}/PolyMC-macOS-#{version}.tar.gz",
-        verified: "github.com/PolyMC/PolyMC/"
-  end
 
   name "PolyMC"
   desc "Minecraft launcher"
